164ca7c4658ddad100df2a1c50d906402fc47e85
abhay2508 <54144891+abhay2508@users.noreply.github.com>
2019-09-11 12:52:43 +0530

    Add files via upload

---
+++ ultrasonic_sensor.ino
@@ -0,0 +1,41 @@
+#include<LiquidCrystal.h>
+const int trigpin = 3;
+const int echopin = 2;
+//LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+void setup() {
+  // put your setup code here, to run once:
+//lcd.begin(16,2);
+//lcd.setCursor(8,8);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+long duration,inches,cm;
+//lcd.setCursor(0,1);
+pinMode(trigpin,OUTPUT);
+digitalWrite(trigpin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigpin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigpin, LOW);
+pinMode(echopin,INPUT);
+duration = pulseIn(echopin,HIGH);
+inches = microsecondsToInches(duration);
+cm=microsecondsToCentimeters(duration);
+Serial.print(inches);
+Serial.print("in, ");
+Serial.print(cm);
+Serial.print("cm");
+
+delay(1000);
+}
+long microsecondsToInches(long microseconds)
+{
+  return microseconds/74/2;
+}
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds/29/2;
+}
